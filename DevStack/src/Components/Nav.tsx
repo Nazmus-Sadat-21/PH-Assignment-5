9d85d36e82c1c513be45ad04ad70e613dd2620a1
Nazmus-Sadat-21 <nazmussadat131@gmail.com>
2026-09-10 22:56:59 +0600

    Navbar updated

--- DevStack/src/Components/Nav.tsx
+++ DevStack/src/Components/Nav.tsx
@@ -3,20 +3,24 @@ export default function Nav() {
     
     return (
         <>
-            <div>
+            <div className="bg-base-100 shadow-lg sticky top-0 z-50 ">
+                <div className="flex justify-between items-center  p-4 container mx-auto">
                 <img src={logo} alt="Dev Stack png" />
                 <div>
-                    <ul>
+                    <ul className="flex gap-6">
                         <li><a href="#">Home</a></li>
+                        <li><a href="#">Technologies</a></li>
+                        <li><a href="#">Projects</a></li>
                         <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
                 </div>
-                <div>
-                    <button>Sign In</button>
-                    <button>Sign Up</button>
+                <div className="flex gap-4">
+                    
+                    <button className="btn btn-white rounded-3xl">Sign In</button>
+                    <button className="btn btn-error text-white rounded-3xl">Sign Up</button>
                 </div>
+            </div>
             </div>
         </>
     )
